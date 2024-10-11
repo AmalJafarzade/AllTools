daf22595b38de1e565a9be76cd7b1dde1448897e
Amal Jafarzade <110107818+AmalJafarzade@users.noreply.github.com>
2024-10-11 17:59:34 +0400

    Delete AllExtensions directory

--- AllExtensions/AllExtensions_for_Google/popup.js
+++
@@ -1,8 +0,0 @@
-// popup.js
-
-// More Information button click event handler
-document.getElementById("infoButton").addEventListener("click", function() {
-    // Open a new tab with more information about clickjacking
-    chrome.tabs.create({ url: "https://en.wikipedia.org/wiki/Clickjacking" });
-  });
-  
